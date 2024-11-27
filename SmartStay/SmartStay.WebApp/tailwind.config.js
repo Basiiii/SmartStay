04b29bc1e57c616fb7355949ab35eefd2e1743b2
Enrique R. (Basi) <hello.basi@proton.me>
2024-11-27 01:07:30 +0000

    Prepare Basic Blazor App
    Created a basic Blazor WebAssembly app, ready to implement the real app.

---
+++ SmartStay/SmartStay.WebApp/tailwind.config.js
@@ -0,0 +1,62 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './**/*.razor', // Razor components
+    './Pages/**/*.razor', // If components are stored in a Pages folder
+    './Shared/**/*.razor', // If components are in a Shared folder
+    './wwwroot/index.html', // Main HTML file
+    "./**/*.{razor,html,cshtml}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+      }
+    },
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
