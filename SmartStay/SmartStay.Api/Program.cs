6a0115239f33efc614cbc78a68fcc194cbf1bf64
Enrique R. (Basi) <hello.basi@proton.me>
2024-12-05 14:37:57 +0000

    Create API Project

---
+++ SmartStay/SmartStay.Api/Program.cs
@@ -0,0 +1,39 @@
+
+using SmartStay.Core.Services;
+
+namespace SmartStay.Api
+{
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        // Register services to the DI container
+        builder.Services.AddSingleton<BookingManager>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
+}
